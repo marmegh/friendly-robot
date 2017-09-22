cfcf1e7ffa3aed3dd52e5a3061eda3da207cf900
MarMegh <martinmm@outlook.com>
2017-09-22 11:55:23 -0700

    Functional Test with classes

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'Login' in browser.title
-
-browser.quit()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_form(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Login', self.browser.title)
+        self.fail('Finish the test!')
+if __name__ == '__main__':
+    unittest.main()
